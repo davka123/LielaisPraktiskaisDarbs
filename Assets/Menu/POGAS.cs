d270dffa870d75d3b2acfd963eee27fe21259d5b
davis <davka@inbox.lv>
2017-05-01 23:00:17 +0200

    2commit

---
+++ Assets/Menu/POGAS.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class POGAS : MonoBehaviour {
+
+public void saktspeli_BTN(string jaunaspele)
+    {
+        SceneManager.LoadScene(jaunaspele);
+    }
+public void beigtspeli_BTN(string beigtspeli)
+    {
+        Application.Quit();
+    }
+}
